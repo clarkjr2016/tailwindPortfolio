454a7f47e9d21944d67f1ff6eb862b3e0ca8116d
theelephantcoder <56000301+clarkjr2016@users.noreply.github.com>
2025-05-13 20:16:14 -0400

    Delete Learning with Leon/class27-materials/class27-materials directory

--- Learning with Leon/class27-materials/class27-materials/local-storage-botScore-button/js/main.js
+++
@@ -1,7 +0,0 @@
-//Create a button that adds 1 to a botScore stored in localStorage
-document.querySelector("button").addEventListener("click", getFetch);
-let img = document.querySelector("img");
-
-function getFetch() {
-  const date = document.querySelector("input").value;
-}
