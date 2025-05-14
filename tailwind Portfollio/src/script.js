c656eca6b64f26dc95a4d6a4d6d76aa923331d08
Eugene Clark <clarkjr2016@gmail.com>
2025-05-14 01:31:42 -0400

    Vibe coded my way out of this one

--- tailwind Portfollio/src/script.js
+++
@@ -1,9 +0,0 @@
-const toggleElement = document.querySelector(
-  ".flex.w-8.h-4.md\\:hidden.flex-col.justify-between"
-);
-
-const mobileLinks = document.querySelector(".hidden.md\\:hidden");
-
-toggleElement.addEventListener("click", () => {
-  mobileLinks.classList.toggle("hidden");
-});
